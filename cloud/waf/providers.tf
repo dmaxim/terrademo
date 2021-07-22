2d7c39c8eeadc663dc235b344c364686482fba85
Dan Maxim <dmaxim@southeastrans.com>
2021-07-22 11:57:54 -0600

    Add workspace terraform shell

--- cloud/waf/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "azurerm" {
-  subscription_id = var.azure-subscription-id
-  client_id       = var.azure-service-principal-id
-  client_secret   = var.azure-service-principal-secret
-  tenant_id       = var.azure-tenant-id
-  features {}
-}
--- cloud/waf/rg.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_resource_group" "waf-rg" {
-  name     = join("-", ["rg", var.namespace])
-  location = var.location
-
-  tags = {
-    environment = var.environment
-  }
-}
