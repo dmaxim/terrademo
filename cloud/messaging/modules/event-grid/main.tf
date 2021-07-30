64218b134a2ebce50bcb9831f9aa7ddd33f84a65
Dan Maxim <dmaxim@southeastrans.com>
2021-07-30 11:58:34 -0600

    Rename topic and deleted topic

--- cloud/messaging/modules/event-grid/main.tf
+++ cloud/messaging/modules/event-grid/main.tf
@@ -1,7 +1,18 @@
 
 
 resource "azurerm_eventgrid_topic" "demo" {
-  name                = join("-", ["egt", var.namespace, var.environment])
+  name                = join("-", ["egt", var.namespace, var.environment, "resource-added"])
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    environment = var.environment
+  }
+
+}
+
+resource "azurerm_eventgrid_topic" "demo_deleted" {
+  name                = join("-", ["egt", var.namespace, var.environment, "resource-deleted"])
   location            = var.location
   resource_group_name = var.resource_group_name
 
